13efda78cccf4e421b8269f8ca07be66bc9da7d9
Mathias Tavernier-Roux <mathias.tavernier-roux@outlook.com>
2020-11-02 16:53:13 +0100

    Envoi : Jour 1 Job 02

---
+++ jour01/job02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack 2 : Jour 01 Job 09</title>
+</head>
+<body>
+    <?php
+    $first = "Hello LaPlateforme!";
+    echo "<h2>$first</h2>";
+    $secondP1 = " LaPlateforme";
+    $secondP2 = "!";
+    $secondP3 = "Hello";
+    $second = "$secondP3 $secondP1$secondP2";
+    echo "<h2>$second</h2>";
+    ?>
+</body>
+</html>
