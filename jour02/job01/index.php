2879b1369d234a2c3fce844fdf1bf6d7b1984d26
Mathias Tavernier-Roux <mathias.tavernier-roux@outlook.com>
2020-11-03 11:59:09 +0100

    Envoi : Jour 2 Job 01

---
+++ jour02/job01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack 2 Jour 2 Job O1</title>
+</head>
+<body>
+    <main>
+    <?php 
+    for ($i=1; $i<1337; $i++) {
+        
+        if ($i == 42)
+        {
+            echo "<b><u>$i</u></b> <br />";
+        }
+        else
+        {
+            echo "$i <br />";
+        }
+    };
+    ?>
+    </main>
+</body>
+</html>
